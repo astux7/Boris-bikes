2e90601d375b0bc6b5c18d2af5afdfdb39da1759
astux7 <Asta.bevainyte@gmail.com>
2013-11-25 13:52:32 +0000

    Docking TDD and class created

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,26 @@
 class DockingStation
-	def initialize
+
+  DEFAULT_CAPACITY = 20
+
+	def initialize(options = {})
+    @capacity = options.fetch(:capacity) || DEFAULT_CAPACITY
 		@bikes = []
 	end
 
 	def dock(bike)
+    rise "Station is full" if full?
 		@bikes << bike
 	end
 
 	def bike_count
 		@bikes.count
 	end
+
+  def full?
+    @bikes.count == @capacity
+  end
+
+  def fill_station(station)
+    20.times {station.dock(Bike.new)}
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,20 @@
 require_relative "../lib/docking_station"
+require_relative "../lib/bike"
 describe DockingStation do
 	let(:bike) {Bike.new}
-	let(:station) {DockingStation.new}
+	let(:station) {DockingStation.new(:capacity => 20)}
 	it "should accecpt a bike" do
 		expect(station.bike_count).to eq(0)
 		expect(station.dock(bike))
 		expect(station.bike_count).to eq(1)
 	end
+
+  it "should know when it full" do
+    expect(station).not_to be_full
+    20.times { station.dock(Bike.new)}
+    expect(station).to be_full
+  end
+
 	
+
 end
