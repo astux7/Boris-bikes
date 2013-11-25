0566d1e74151bbb8481a9dce017f82bf44f6a1d5
astux7 <Asta.bevainyte@gmail.com>
2013-11-25 13:04:32 +0000

    we can break a bike, fix it too and check its status

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,5 +5,13 @@ describe Bike do
     #expect(bike).not_to be_broken
     expect(bike.broken?).to be_false
   end	
-
+  it "should be able to break" do
+    bike.break
+    expect(bike).to be_broken
+  end
+  it "should be able to get fixed" do
+    bike.break
+    bike.fix
+    expect(bike).not_to be_broken
+  end
 end
